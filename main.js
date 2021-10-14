3cdb42438b9c4e76f3cc19e5ecc6f4949e1418e0
zqsd <17475349+zqsd1@users.noreply.github.com>
2021-10-14 19:07:21 +0200

    canvas animation

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+//#region variables
+const CANVAS_WIDTH = 500
+const CANVAS_HEIGHT = 300
+
+const FPS = 60
+const INTERVAL = 1000 / FPS
+const U_SPEED = 20
+
+
+//#endregion
+
+
+
 /**
  * creer un HTMLElement
  * @param {string} name 
@@ -29,7 +42,7 @@ function createHTML(name, attributes, parent, content) {
 
 
 
-const canvas = createHTML('canvas', { id: "xxx", class: 'zaer', width: 500, height: 500 }, document.body)
+const canvas = createHTML('canvas', { id: "canvas", class: "mycanvas", width: CANVAS_WIDTH, height: CANVAS_HEIGHT }, document.body)
 const buttonGenerate = createHTML('button', { id: 'btnGenerate' }, document.body, "GENERATE")
 const buttonSolve = createHTML('button', { id: 'btnSolve' }, document.body, "SOLVE")
 const ctx = canvas.getContext('2d')
@@ -42,5 +55,59 @@ function solve(evt) {
 }
 
 function generate(evt) {
+    togglePause()
     console.log('generate');
 }
+
+
+//#region animation
+let animationFrame, lastFrameTime, lastUpdateTime
+
+
+function gameLoop(timestamp) {
+    animationFrame = window.requestAnimationFrame(gameLoop)
+
+    const timeSinceLastFrame = timestamp - lastFrameTime
+    const timeSincelLastUpdate = timestamp - lastUpdateTime
+
+    if (timeSinceLastFrame > INTERVAL) {
+        draw()
+        lastFrameTime = timestamp
+    }
+
+    if (timeSincelLastUpdate > 1000 / U_SPEED) {
+        update()
+        lastUpdateTime = timestamp
+    }
+}
+
+
+function play() {
+    if (animationFrame === undefined) {
+        animationFrame = window.requestAnimationFrame(gameLoop)
+        lastUpdateTime = lastFrameTime = performance.now() // 0 ?
+        // draw()
+        console.log("play");
+    }
+}
+
+function togglePause() {
+
+    if (animationFrame != undefined) {
+        window.cancelAnimationFrame(animationFrame)
+        animationFrame = undefined
+        console.log('pause');
+    } else {
+        play()
+
+    }
+}
+
+function draw() {
+ctx.fillRect(50,50,100,10)
+}
+
+function update() {
+
+}
+//#endregion
